02b40b9381a5ef6f37e55b40caaf3b50cc66e227
gabrielemafrica <gabriele.ga@live.it>
2023-05-03 20:37:20 +0300

    finale dado

--- dadi/js/myjs.js
+++ dadi/js/myjs.js
@@ -2,26 +2,49 @@
 
 const tiradado = document.getElementById('tiradado');
 
+//prendo i contenitori
+
+const risultatoUser = document.getElementById('risultatoUser');
+const risultatoPc = document.getElementById('risultatoPc');
+const risultato = document.getElementById('risultato');
+
 //tirare il dado
 tiradado.addEventListener('click',
     function() {
-        //genero unamero user
+        //genero numero user
         let numeroDadoUser = Math.floor(Math.random() * 6) + 1;
        
 
 
-        //genero unamero pc
+        //genero numero pc
         let numeroDadoPc = Math.floor(Math.random() * 6) + 1;
+
+        //risultato user
+        const nuovoNumUser = `<h1>${numeroDadoUser}</h1>`;
+        risultatoUser.innerHTML = nuovoNumUser;
+        //risultato pc
+        const nuovoNumPc = `<h1>${numeroDadoPc}</h1>`;
+        risultatoPc.innerHTML = nuovoNumPc;
 
 
         console.log(`il numero user é ${numeroDadoUser}`);
         console.log(`il numero del PC é ${numeroDadoPc}`);
 
+        //vice user
        if (numeroDadoUser > numeroDadoPc) {
+        const vittoria = `<h1>HAI VINTO!</h1>`;
+        risultato.innerHTML = vittoria;
+
             console.log(`HAI VINTO!`);
+        //vince pc
        }else if (numeroDadoUser < numeroDadoPc) {
+        const vittoria = `<h1>HAI PERSO!</h1>`;
+        risultato.innerHTML = vittoria;
             console.log(`HAI PERSO!`);
+        //pareggio
        }else {
+            const vittoria = `<h1>PARI!</h1>`;
+            risultato.innerHTML = vittoria;
             console.log(`PAREGGIO`);
        }
 
